9f7b214ce146c68ee55e1fe4d4083cab150b2174
SlouchyBeanie <brady.anderson27@gmail.com>
2017-06-25 19:53:26 -0500

    Added post list view

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+
+class PublishedManager(models.Manager):
+	def get_queryset(self):
+		return super(PublishedManager, self).get_queryset().filter(status='published')
 
 class Post(models.Model):
 	STATUS_CHOICES = (
@@ -15,6 +20,15 @@ class Post(models.Model):
 	created = models.DateTimeField(auto_now_add=True)
 	updated = models.DateTimeField(auto_now_add=True)
 	status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+
+	objects = models.Manager()
+	published = PublishedManager()
+
+	def get_absolute_url(self):
+		return reverse('blog:post_detail', args=[self.publish.year,
+												 self.publish.strftime('%m'),
+												 self.publish.strftime('%d'),
+												 self.slug])
 
 	class Meta:
 		ordering = ('-publish',)
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
-# Create your views here.
+def post_list(request):
+	posts = Post.published.all()
+	
+	context = {
+		'posts' : posts,
+	}
+
+	return render(request, 'blog/post/list.html', context)
+
+def post_detail(request, year, month, day, post):
+	post = get_object_or_404(Post, slug=post,
+								   status = 'published',
+								   publish__year=year,
+								   publish__month=month,
+								   publish__day=day)
+	
+	context = {
+		'post': post,
+	}
+	
+	return render(request, 'blog/post/detail.html', context)
