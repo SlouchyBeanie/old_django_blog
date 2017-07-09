c275fee7553881aa846bcfabe0dae42db9cd39b9
SlouchyBeanie <brady.anderson27@gmail.com>
2017-07-09 16:29:19 -0500

    commit

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, get_object_or_404
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+#from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 from .models import Post
+from .forms import EmailPostForm
 
-def post_list(request):
+'''def post_list(request):
 	object_list = Post.published.all()
 	paginator = Paginator(object_list, 3) # 3 posts in each page
 	page = request.GET.get('page')
@@ -21,7 +23,14 @@ def post_list(request):
 		'posts': posts,
 	}
 
-	return render(request, 'blog/post/list.html', context)
+	return render(request, 'blog/post/list.html', context)'''
+
+class PostListView(ListView):
+	queryset = Post.published.all()
+	context_object_name = 'posts'
+	paginate_by = 3
+	template_name = 'blog/post/list.html'
+
 
 def post_detail(request, year, month, day, post):
 	post = get_object_or_404(Post, slug=post,
@@ -35,3 +44,25 @@ def post_detail(request, year, month, day, post):
 	}
 	
 	return render(request, 'blog/post/detail.html', context)
+
+
+def post_share(request, post_id):
+	# Retrieve post by id
+	post = get_object_or_404(Post, id=post_id, status='published')
+
+	if request.method == "POST":
+		# Form was submitted
+		form = EmailPostForm(request.POST)
+		if form.is_valid():
+			# Form fields passed validation
+			cd = form.cleaned_data
+			# ... send email
+		else:
+			form = EmailPostForm()
+
+		context = {
+			'page' : page,
+			'posts' : posts,
+		}
+
+		return render(request, 'blog/post/share.html', context)
