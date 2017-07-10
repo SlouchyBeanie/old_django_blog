ed8d2fa141331d6d977153521de3fcdf6b93b848
SlouchyBeanie <brady.anderson27@gmail.com>
2017-07-09 22:27:19 -0500

    Added a comment system

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, get_object_or_404
-#from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from django.core.mail import send_mail
-from .models import Post
-from .forms import EmailPostForm
+from .models import Post, Comment
+from .forms import EmailPostForm, CommentForm
 
 '''def post_list(request):
 	object_list = Post.published.all()
@@ -40,8 +40,26 @@ def post_detail(request, year, month, day, post):
 								   publish__month=month,
 								   publish__day=day)
 	
+	# List of active comments for this post
+	comments = post.comments.filter(active=True)
+
+	if request.method == 'POST':
+		# A comment was posted
+		comment_form = CommentForm(data=request.POST)
+		if comment_form.is_valid():
+			# Create Comment object but don't save to database yet
+			new_comment = comment_form.save(commit=False)
+			# Assign the current post to the comment
+			new_comment.post = post 			#new_comment.post != above post. This is the post in the Comment Model
+			# Save the comment to the database
+			new_comment.save()
+	else:
+		comment_form = CommentForm()
+
 	context = {
 		'post': post,
+		'comments': comments,
+		'comment_form': comment_form,
 	}
 	
 	return render(request, 'blog/post/detail.html', context)
